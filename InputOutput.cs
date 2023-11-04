a87d18f7bcca30858912bd3e0d614169bb350cdc
Nataliia Andrieieva <natalia__forever@ukr.net>
2023-11-04 18:01:15 +0200

    Create InputOutput.cs

---
+++ InputOutput.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Linq;
+
+class InputOutput
+{
+    public Direction GetNewDirection()
+    {
+        if (Console.KeyAvailable)
+        {
+            var key = Console.ReadKey(true).Key;
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    return Direction.Up;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    return Direction.Down;
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    return Direction.Left;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    return Direction.Right;
+            }
+        }
+        return Direction.NoChange;
+    }
+}
+
+enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    NoChange
+}
