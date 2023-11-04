96d5a8c124bbe1ea83eeb71e5ecd42cfaedc5a48
Nataliia Andrieieva <natalia__forever@ukr.net>
2023-11-04 17:56:23 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WindowHeight = 20;
+        Console.WindowWidth = 44;
+        int screenwidth = Console.WindowWidth;
+        int screenheight = Console.WindowHeight;
+
+        var game = new SnakeGame(screenwidth, screenheight);
+        game.Run();
+    }
+}
