b75c490752325a269464a47cf5004fd15db57a8d
Nataliia Andrieieva <natalia__forever@ukr.net>
2023-11-04 18:03:12 +0200

    Create HighScoreEntry.cs

---
+++ HighScoreEntry.cs
@@ -0,0 +1,21 @@
+﻿[Serializable]
+public class HighScoreEntry
+{
+    public string PlayerName { get; set; }
+    public int Score { get; set; }
+
+    public HighScoreEntry(string playerName, int score)
+    {
+        PlayerName = playerName;
+        Score = score;
+    }
+}
+
+
+
+
+
+
+
+
+
